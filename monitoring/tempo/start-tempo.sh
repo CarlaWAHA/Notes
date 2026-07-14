31002abcc4812f696bc35e6e3a5058f59e0426b4
Carla Waha <estellewaha@gmail.com>
2026-07-14 21:26:07 +0200

    ajustement des blocs render fail

--- monitoring/tempo/start-tempo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-: "${PORT:=3200}"
-
-cp /etc/tempo/tempo.render.yml.template /tmp/tempo.yml
-sed -i "s|__PORT__|${PORT}|g" /tmp/tempo.yml
-
-exec /tempo -config.file=/tmp/tempo.yml
