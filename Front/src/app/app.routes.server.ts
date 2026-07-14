ba733bc1f2e3de766a570be86c0cdf2e5ca09565
Carla Waha <estellewaha@gmail.com>
2026-07-14 15:03:35 +0200

    ajoutement des tests E2E

--- Front/src/app/app.routes.server.ts
+++ Front/src/app/app.routes.server.ts
@@ -2,6 +2,34 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admin',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'student',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'notes',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'notes/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Client
   }
