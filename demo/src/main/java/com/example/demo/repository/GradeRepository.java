ac3b7a1714f6a64a7693b9317b08915e7a8c8e40
Carla Waha <estellewaha@gmail.com>
2026-07-14 11:30:04 +0200

    ajout des authorisation sur la repository

---
+++ demo/src/main/java/com/example/demo/repository/GradeRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Grade;
+import com.example.demo.model.Student;
+import com.example.demo.model.UE;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GradeRepository extends JpaRepository<Grade, Long> {
+    List<Grade> findByStudent(Student student);
+    Optional<Grade> findByStudentAndUe(Student student, UE ue);
+}
