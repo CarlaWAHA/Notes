fbc9c86b05b3f589ff2417d0fd9b0a8ae3231e4c
Carla Waha <estellewaha@gmail.com>
2026-07-14 11:44:36 +0200

    amélioration des opération du role admin

--- demo/src/main/java/com/example/demo/security/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.example.demo.security;
 
 import com.example.demo.service.TokenService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.*;
 import java.util.List;
 
 @Configuration
